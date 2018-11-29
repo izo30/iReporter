0fd74809f09239f0b944e93c7bdaca503da8cfbd
izo30 <isaacwangethi30@gmail.com>
2018-11-30 00:08:01 +0300

    [Feature #162320207] Login a user

--- app/api/v1/views/user_auth_views.py
+++ app/api/v1/views/user_auth_views.py
@@ -11,8 +11,8 @@ parser.add_argument('email', help = 'This field cannot be blank', required = Tru
 parser.add_argument('phone', help = 'This field cannot be blank')
 parser.add_argument('username', help = 'This field cannot be blank')
 parser.add_argument('password', help = 'This field cannot be blank', required = True)
-parser.add_argument('is_admin', help = 'This field cannot be blank')
-parser.add_argument('registered_on', help = 'This field cannot be blank', required = True)
+parser.add_argument('role', help = 'This field cannot be blank')
+parser.add_argument('registered_on', help = 'This field cannot be blank')
 
 
 signup_fields = api.model('Signup', {
@@ -22,7 +22,7 @@ signup_fields = api.model('Signup', {
     'phone' : fields.String,
     'username' : fields.String,
     'password': fields.String,
-    'is_admin': fields.Boolean,
+    'role': fields.String,
     'registered_on': fields.DateTime
 })
 
@@ -38,13 +38,13 @@ class Signup(Resource):
         phone = args['phone']
         username = args['username']
         password = args['password']
-        is_admin = args['is_admin']
+        role = args['role']
         registered_on = args['registered_on']
         
         found_email = User.get_single_user(self, email)
         if found_email == 'not found':
             try:    
-                new_user = User(first_name, last_name, email, phone, username, password, is_admin, registered_on)
+                new_user = User(first_name, last_name, email, phone, username, User.generate_hash(password), role, registered_on)
                 created_user = new_user.create_user()
                 return make_response(jsonify({
                     'status': 'ok',
@@ -62,3 +62,45 @@ class Signup(Resource):
             'status': 'fail',
             'message' : 'Email already exists, please log in'
         }))
+
+login_fields = api.model('Login', {
+    'email': fields.String,
+    'password': fields.String
+})
+"""user login"""
+@api.route('/login')
+class Login(Resource):
+    @api.expect(login_fields)
+    def post(self):
+        args = parser.parse_args()
+        email = args['email']
+        password = args['password']
+                
+        try:
+            current_user = User.get_single_user(self, email)
+            if current_user == 'not found':
+                return make_response(jsonify({
+                    'status': 'success',
+                    'message': 'User does not exist, sign up!'
+                }), 200)
+            if current_user and User.verify_hash(password, current_user['password']):
+                role = current_user['role']
+                email = current_user['email']
+                auth_token = User.encode_auth_token(email, role)   
+                if auth_token:
+                    return make_response(jsonify({
+                        'status' : 'ok',
+                        'message' : 'Logged in successfully',
+                        'auth_token': auth_token.decode()
+                    }), 200) 
+            else:
+                return make_response(jsonify({
+                    'message' : 'Incorrect email or password',
+                    'status' : 'fail'
+                }), 400)
+
+        except Exception as e:
+            return make_response(jsonify({
+                'message' : str(e),
+                'status' : 'failed'
+            }), 500)

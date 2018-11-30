6b2917b9f53863ac76e8eba70409b1fba01bfd8b
izo30 <isaacwangethi30@gmail.com>
2018-11-30 09:21:33 +0300

    [Feature #162250919] Create an incident

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 
 # Import all endpoints for all models
 from .views.user_auth_views import api as user_auth_namespace
+from .views.incident_view import api as incident_namespace
 
 authorizations = {
     'apikey': {
@@ -16,3 +17,4 @@ version1 = Blueprint('api version 1', __name__, url_prefix='/api/v1')
 api = Api(version1, title='iReporter API', version='1.0', description='An application that helps users report corruption incidents and ask for government interventions', authorizations=authorizations)
 
 api.add_namespace(user_auth_namespace, path='/auth')
+api.add_namespace(incident_namespace, path='/incidents')
